c6e14f8a258be85ae776c927ef90760195b18f03
Amar Gill <amar.s.gill@outlook.com>
2020-04-22 04:01:57 -0400

    Can render user report data in about page.

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -3,14 +3,52 @@
 </svelte:head>
 
 <script>
-	import {User} from 'sveltefire';
+	import { User, Doc } from 'sveltefire';
 </script>
 
 <User let:user let:auth>
-	<h1>About this site</h1>
 	
-	<p>User ID: {user.uid}</p>
-	
-	<p>This is the 'about' page. There's not much here.</p>
+	<div>
+		<h1>User Report Data</h1>
+		<p>User ID: {user.uid}</p>
 
+		<Doc path={`reports/${user.uid}`} let:data let:ref log>
+			{#each data.completedGames as game}
+				<p>Game: {game.game}</p>
+				<ul>
+					<li>Completed: {game.completedAt.toDate()}</li>
+					<li>Time (seconds): {game.time}</li>
+				</ul>
+			{/each}
+	
+			<span slot="loading">Loading...</span>
+			<span slot="fallback">
+				Initialize report document for user: {user.uid}
+				<button
+				on:click="{() => {
+					ref.set({
+						uid: user.uid,
+						createdAt: firebase.firestore.Timestamp.fromDate(new Date()),
+						lastActivity: firebase.firestore.Timestamp.fromDate(new Date()),
+					  });
+				}}">
+				Create Document
+			  </button>
+			</span>
+		</Doc>
+
+	</div>
 </User>
+
+<style>
+	div {
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+		color: lightgoldenrodyellow;
+        background-color: lightslategrey;
+        /* background-image: linear-gradient(darkslategray 10%, lightslategrey 40%, white 90%); */
+        border-radius: 6px;
+		overflow: scroll;
+    }
+</style>
