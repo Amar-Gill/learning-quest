85450ca7fb23415e19331b9c92826960ede69cca
Amar Gill <39676845+Amar-Gill@users.noreply.github.com>
2020-04-23 23:53:18 -0400

    Update _layout.svelte

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -6,14 +6,14 @@
 
 	let firebaseConfig = {
 		// Insert Firebase Credentials here
-		apiKey: "AIzaSyDEETmdf3X5Z8ble8bDFippCQ_f11oYzx0",
-		authDomain: "learning-quest.firebaseapp.com",
-		databaseURL: "https://learning-quest.firebaseio.com",
-		projectId: "learning-quest",
-		storageBucket: "learning-quest.appspot.com",
-		messagingSenderId: "253565369099",
-		appId: "1:253565369099:web:4d02f5ac18e6eeb88c800e",
-		measurementId: "G-T5RSBRYPY9"
+		apiKey: "",
+		authDomain: "",
+		databaseURL: "",
+		projectId: "",
+		storageBucket: "",
+		messagingSenderId: "",
+		appId: "",
+		measurementId: ""
 	};
 
 	// hack for bundling errors. see github issue:
