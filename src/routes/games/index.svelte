fd853b2e0becf1593238683b2da79b9cd03dad25
Amar Gill <amar.s.gill@outlook.com>
2020-05-04 16:58:30 -0400

    basic styling of games index page.

--- src/routes/games/index.svelte
+++ src/routes/games/index.svelte
@@ -3,8 +3,35 @@
 
 </script>
 
-<h1>Games</h1>
-
 <div>
+
+    <h1>Games</h1>
+
     <a href="games/add" alt="add">Basic Addition</a>
 </div>
+
+<style>
+    div {
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+        color: lightgoldenrodyellow;
+        background-color: lightslategrey;
+        /* background-image: linear-gradient(darkslategray 10%, lightslategrey 40%, white 90%); */
+        border-radius: 6px;
+        padding-left: 2rem;
+    }
+
+    a:link,
+    a:visited {
+        color:lightgoldenrodyellow;
+    }
+
+    a:hover {
+        color:burlywood;
+    }
+
+    a:active {
+        color: blueviolet;
+    }
+</style>
