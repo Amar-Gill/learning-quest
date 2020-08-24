6e0a9868dc98f85399ec9c9f08dfde5f835fab1a
Amar Gill <amar.s.gill@outlook.com>
2020-08-24 13:26:08 -0400

    redid home page.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,48 +1,24 @@
-<style>
-	div {
-		display: flex;
-		align-items: flex-end;
-		height: 100vh;
-		background-image: linear-gradient(black 20%, darkslategrey 40%, white 90%);
-		border-radius: 6px;
-	}
-
-	img {
-		width: 100%;
-		max-width: 240px;
-	}
-
-	figure {
-		animation-duration: 3s;
-		animation-name: slidein;
-		animation-fill-mode: forwards;
-	}
-
-	@keyframes slidein {
-
-		to {
-			/* margin-left: 78%; */
-			transform: translateX(68vw);
-			/* width: 100%; */
-		}
-	}
-
-	h1 {
-		position: fixed;
-		color: lightgoldenrodyellow;
-		left: 7rem;
-	}
-</style>
-
-<svelte:head>
-	<title>Home</title>
-</svelte:head>
-
-<h1>Learning-Quest 0.1.0</h1>
-
 <div>
-	<figure id='fig'>
-		<img alt='ruby-light' src='images/Carbuncle.png'>
-		<!-- <figcaption>Carbuncle casts Ruby Light</figcaption> -->
-	</figure>
+
+  <h1>Learning Quest 0.1.0</h1>
+
+  <p>
+    This is an online education platform for children between the ages of 5-10.
+  </p>
+  <p>
+    The learning happens by playing various games which are designed to teach
+    children arithmetic and logic.
+  </p>
+
+  <h2>How to Play</h2>
+  <p>
+    Visit the
+    <a href="/games" alt="games link">games</a>
+    page to select from a list of available games.
+  </p>
+  <p>
+    Visit the
+    <a href="/activity" alt="activity link">activity</a>
+    page to review your completed games and performance data.
+  </p>
 </div>
